d044ac65f953c30632b2219af627d0fc92e492fa
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2024-09-30 09:39:08 -0500

    more robust eval loop

--- src/evaluate_ab.py
+++ src/evaluate_ab.py
@@ -19,19 +19,26 @@ with open(prompts_path / "system.txt", "r") as f:
     prompt_system = f.read()
 
 con = duckdb.connect(database=":memory:")
-# load sample of upworthy data
-df = con.execute("""WITH sampled_tests AS (
-    SELECT DISTINCT clickability_test_id FROM 'data/upworthy_processed.csv' USING SAMPLE 2
-    )
-    SELECT * FROM 'data/upworthy_processed.csv' WHERE clickability_test_id IN (SELECT clickability_test_id FROM sampled_tests)
-    """).fetchdf()
-# for each test
-test_ids = df["clickability_test_id"].unique()
 
-results_single = []
-results_multi = []
-results_cot = []
-results_reasoning = []
+# check if test ID path exists
+if not Path("./data/sample_ids.csv").exists():
+    # load sample of upworthy data
+    df = con.execute("""WITH sampled_tests AS (
+        SELECT DISTINCT clickability_test_id FROM 'data/upworthy_processed.csv' USING SAMPLE 50
+        )
+        SELECT * FROM 'data/upworthy_processed.csv' WHERE clickability_test_id IN (SELECT clickability_test_id FROM sampled_tests)
+        """).fetchdf()
+    # for each test
+    test_ids = df["clickability_test_id"].unique()
+
+    pd.DataFrame(test_ids, columns=["test_id"]).to_csv(
+        "./data/sample_ids.csv", index=False
+    )
+else:
+    test_ids = pd.read_csv("./data/sample_ids.csv")["test_id"].tolist()
+    df = con.execute(
+        """SELECT * FROM 'data/upworthy_processed.csv' WHERE clickability_test_id IN (SELECT test_id FROM 'data/sample_ids.csv')"""
+    )
 
 
 def process_model_response(response, winning_idx):
@@ -41,7 +48,7 @@ def process_model_response(response, winning_idx):
         elif "content" in response:
             try:
                 resp_id = int(response["content"].split("ID: ")[1].split("\n")[0])
-            except (IndexError, ValueError):
+            except (IndexError, ValueError, AttributeError):
                 return None
         else:
             return None
@@ -59,15 +66,33 @@ def process_model_response(response, winning_idx):
     return correct
 
 
+def check_for_results(res_path):
+    if not res_path.exists():
+        return pd.DataFrame(columns=["test_id", "is_correct", "response", "winner"])
+    else:
+        return pd.read_csv(res_path)
+
+
 models = [
-    # "gpt-4o-mini",
-    # "llama3.1",
-    # "gpt-4o",
+    "gpt-4o-mini",
+    "llama3.1",
+    "gpt-4o-2024-08-06",
     "o1-mini",
-    # "o1-preview"
+    "o1-preview",
 ]
 
 for model in models:
+    output_path = prompts_path / "output" / model
+    output_path.mkdir(parents=True, exist_ok=True)
+
+    results_single = check_for_results(output_path / "single_results.csv")
+    results_multi = check_for_results(output_path / "multi_results.csv")
+    results_cot = check_for_results(output_path / "cot_results.csv")
+    results_reasoning = check_for_results(output_path / "reasoning_results.csv")
+
+    test_ids = [i for i in test_ids if i not in results_single.test_id]
+
+    print(f"Running evaluation for model: {model}")
     for test_id in tqdm(test_ids):
         variants = df[df["clickability_test_id"] == test_id]
         # randomly shuffle variants
@@ -98,43 +123,51 @@ for model in models:
             model, prompt_single, prompt_multi_copy, prompt_system
         )
 
-        results_single.append(
+        # write or append rows to corresponding CSVs
+        pd.DataFrame(
             {
+                "test_id": [test_id],
                 "is_correct": process_model_response(single, winning_idx),
                 "response": single,
                 "winner": winning_idx,
             }
+        ).to_csv(
+            output_path / "single_results.csv", mode="a", header=False, index=False
         )
 
         if model not in ["o1-mini", "o1-preview"]:
-            results_multi.append(
+            pd.DataFrame(
                 {
+                    "test_id": [test_id],
                     "is_correct": process_model_response(multi, winning_idx),
                     "response": multi,
                     "winner": winning_idx,
                 }
+            ).to_csv(
+                output_path / "multi_results.csv", mode="a", header=False, index=False
             )
-            results_cot.append(
+
+            pd.DataFrame(
                 {
+                    "test_id": [test_id],
                     "is_correct": process_model_response(cot, winning_idx),
                     "response": cot,
                     "winner": winning_idx,
                 }
+            ).to_csv(
+                output_path / "cot_results.csv", mode="a", header=False, index=False
             )
-            results_reasoning.append(
+
+            pd.DataFrame(
                 {
-                    "correct": process_model_response(reasoning, winning_idx),
+                    "test_id": [test_id],
+                    "is_correct": process_model_response(reasoning, winning_idx),
                     "response": reasoning,
                     "winner": winning_idx,
                 }
+            ).to_csv(
+                output_path / "reasoning_results.csv",
+                mode="a",
+                header=False,
+                index=False,
             )
-
-    output_path = prompts_path / "output" / model
-    output_path.mkdir(exist_ok=True, parents=True)
-
-    pd.DataFrame(results_single).to_csv(output_path / "single_results.csv", index=False)
-    pd.DataFrame(results_multi).to_csv(output_path / "multi_results.csv", index=False)
-    pd.DataFrame(results_cot).to_csv(output_path / "cot_results.csv", index=False)
-    pd.DataFrame(results_reasoning).to_csv(
-        output_path / "reasoning_results.csv", index=False
-    )
